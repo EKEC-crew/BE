a2c2b4ae6f6231f548eb3678b5713edfd27876ce
Ksieon <keumsiun0503@gmail.com>
2025-07-10 21:19:46 +0900

    :sparkles: Feat: dto update

---
+++ src/crew/page/post/dto/request/post.request.dto.js
@@ -0,0 +1,23 @@
+export const createCrewPostRequest = (body) => {
+  return{
+
+  }
+}
+
+export const updateCrewPostRequest = (body) => {
+    return{
+
+    }
+}
+
+export const createCrewPostCommentRequest = (body) => {
+    return{
+
+    }
+}
+
+export const updateCrewPostCommentRequest = (body) => {
+    return{
+
+    }
+}

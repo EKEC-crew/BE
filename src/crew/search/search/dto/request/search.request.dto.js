e3b1a0fa145698d744717d65fcb42705604882b3
ほし <43630612+hoshi2710@users.noreply.github.com>
2025-07-16 17:28:40 +0900

    ✨Feat: 크루 검색 필터 (new)

--- src/crew/search/search/dto/request/search.request.dto.js
+++ src/crew/search/search/dto/request/search.request.dto.js
@@ -1,11 +1,27 @@
+/**
+ * **[Crew Search]**
+ * **\<Request DTO>\>**
+ * ***bodyToDefaultSearch***
+ * '크루명으로 검색'기능의 요청 값을 서비스 레이어로 옮기기 위한 DTO
+ * @param {Object} query 
+ * @returns}
+ */
 export const bodyToDefaultSearch = (query) => {
     return {
         name: query.name,
-        page: parseInt(query.page)
+        page: parseInt(query.page),
+        sort: parseInt(query.sort),
     }
 }
+/**
+ * **[Crew Search]**
+ * **\<Request DTO>\>**
+ * ***bodyToAdvancedSearch***
+ * '크루 찾아보기 (고급 검색)'기능의 요청 값을 서비스 레이어로 옮기기 위한 DTO
+ * @param {Object} query 
+ * @returns}
+ */
 export const bodyToAdvancedSearch = (query) => {
-    console.log(query)
     return {
         name: query.name,
         category: query.category != undefined ? parseInt(query.category) : undefined,
@@ -14,6 +30,7 @@ export const bodyToAdvancedSearch = (query) => {
         region: query.region != undefined ? parseInt(query.region) : undefined,
         age: query.age != undefined ? parseInt(query.age) : undefined,
         gender: query.gender != undefined ? parseInt(query.gender) : undefined,
-        page: parseInt(query.page)
+        page: parseInt(query.page),
+        sort: parseInt(query.sort),
     }
 }
--- src/crew/search/search/route/search.route.js
+++ src/crew/search/search/route/search.route.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import { handleAdvancedSearch, handleDefaultSearch } from '../controller/search.controller.js';
 const router = express.Router();
-router.get('/name', handleDefaultSearch);
-router.get('/detail', handleAdvancedSearch);
+router.get('/name', handleDefaultSearch); // 크루명으로 검색
+router.get('/detail', handleAdvancedSearch); // 크루 찾아보기 (고급 검색)
 export default router;

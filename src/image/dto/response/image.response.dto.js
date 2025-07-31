6f7503ffbe25a13f5be795af9a4c2166a20726e1
ほし <43630612+hoshi2710@users.noreply.github.com>
2025-07-31 20:56:44 +0900

    :recycle: Refactor: s3로 이미지 업로드 공통함수 리펙토링 #92 - 완료

--- src/image/dto/response/image.response.dto.js
+++ src/image/dto/response/image.response.dto.js
@@ -1,13 +1,13 @@
 /**
  * **[Crew Search]**
  * **\<🧺⬆️ Response DTO\>**
- * ***responseFromGetImageURL***
+ * ***responseFromGetImage***
  * '이미지 로드'기능의 요청 결과값을 서비스 레이어에서 컨트롤러로 반환하기 위한 DTO
  * @param {Object} data
  * @returns {Object}
  */
-export const responseFromGetImageURL = (data) => {
-    return {
-        url: data.getSignedUrlForGet
-    }
-}
+export const responseFromGetImage = (data) => {
+  return {
+    url: data.imageUrl,
+  };
+};

8374709cdea33704210a99b5576e199e0663d19c
Ksieon <keumsiun0503@gmail.com>
2025-08-01 15:17:42 +0900

    :sparkles: Feat: crew-page-album

--- src/crew/page/album/route/album.route.js
+++ src/crew/page/album/route/album.route.js
@@ -1,4 +1,15 @@
 import express from 'express';
-const router = express.Router();
-router.get('/', (req, res) => res.send('Hello UMC!'));
+import { verifyToken } from "../../../../middleware/authMiddleware.js";
+import * as albumController from "../controller/album.controller.js";
+import multer from 'multer';
+
+const router = express.Router({ mergeParams: true });
+
+const upload = multer({
+    storage: multer.memoryStorage()
+});
+
+router.get('/', albumController.readAlbumImages);
+router.post('/', upload.single('image'), albumController.createAlbumImage);
+
 export default router;

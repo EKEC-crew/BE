2b47ea28fca8332aca82f121deda326f9139cd00
Sieon Keum <keumsiun0503@gmail.com>
2025-08-06 00:43:26 +0900

    :recycle: Refactor: add middleware

--- src/crew/page/post/route/post.route.js
+++ src/crew/page/post/route/post.route.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { verifyToken } from "../../../../middleware/authMiddleware.js";
+import { authenticateAccessToken } from '../../../../auth/middleware/auth.middleware.js';
 import * as postController from "../controller/post.controller.js";
 import multer from 'multer';
 
@@ -10,14 +10,14 @@ const upload = multer({
 });
 
 router.get('/list', postController.readPostsByCrew);
-router.post('/', upload.array('images', 5), postController.createCrewPost); //인증 필요
+router.post('/', authenticateAccessToken, upload.array('images', 5), postController.createCrewPost); //인증 필요
 router.get('/:postId', postController.readCrewPost);
-router.put('/:postId', upload.array('images', 10), postController.updateCrewPost); //인증 필요
-router.delete('/:postId', postController.deleteCrewPost); //인증 필요
-router.post('/:postId/like', postController.toggleCrewPostLike); //인증 필요
+router.put('/:postId', authenticateAccessToken, upload.array('images', 10), postController.updateCrewPost); //인증 필요
+router.delete('/:postId', authenticateAccessToken, postController.deleteCrewPost); //인증 필요
+router.post('/:postId/like', authenticateAccessToken, postController.toggleCrewPostLike); //인증 필요
 router.get('/:postId/comment/list', postController.readCommentsByCrewPost);
-router.post('/:postId/comment', postController.createCrewPostComment); //인증 필요
-router.put('/:postId/comment/:commentId', postController.updateCrewPostComment); //인증 필요
-router.delete('/:postId/comment/:commentId', postController.deleteCrewPostComment); //인증 필요
+router.post('/:postId/comment', authenticateAccessToken, postController.createCrewPostComment); //인증 필요
+router.put('/:postId/comment/:commentId', authenticateAccessToken, postController.updateCrewPostComment); //인증 필요
+router.delete('/:postId/comment/:commentId', authenticateAccessToken, postController.deleteCrewPostComment); //인증 필요
 
 export default router;

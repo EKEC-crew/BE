ca962db3531dce40c048dcf40bed295d852404d4
Sieon Keum <keumsiun0503@gmail.com>
2025-08-06 10:48:48 +0900

    :recycle: Refactor: 인증 미들웨어 추가

--- src/crew/page/post/route/post.route.js
+++ src/crew/page/post/route/post.route.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { authenticateAccessToken } from '../../../../auth/middleware/auth.middleware.js';
+import { authenticateAccessToken, verifyUserIsActive } from '../../../../auth/middleware/auth.middleware.js';
 import * as postController from "../controller/post.controller.js";
 import multer from 'multer';
 
@@ -10,14 +10,14 @@ const upload = multer({
 });
 
 router.get('/list', postController.readPostsByCrew);
-router.post('/', authenticateAccessToken, upload.array('images', 5), postController.createCrewPost); //인증 필요
+router.post('/', authenticateAccessToken, verifyUserIsActive, upload.array('images', 5), postController.createCrewPost); //인증 필요
 router.get('/:postId', postController.readCrewPost);
-router.put('/:postId', authenticateAccessToken, upload.array('images', 10), postController.updateCrewPost); //인증 필요
-router.delete('/:postId', authenticateAccessToken, postController.deleteCrewPost); //인증 필요
-router.post('/:postId/like', authenticateAccessToken, postController.toggleCrewPostLike); //인증 필요
+router.put('/:postId', authenticateAccessToken, verifyUserIsActive, upload.array('images', 10), postController.updateCrewPost); //인증 필요
+router.delete('/:postId', authenticateAccessToken, verifyUserIsActive, postController.deleteCrewPost); //인증 필요
+router.post('/:postId/like', authenticateAccessToken, verifyUserIsActive, postController.toggleCrewPostLike); //인증 필요
 router.get('/:postId/comment/list', postController.readCommentsByCrewPost);
-router.post('/:postId/comment', authenticateAccessToken, postController.createCrewPostComment); //인증 필요
-router.put('/:postId/comment/:commentId', authenticateAccessToken, postController.updateCrewPostComment); //인증 필요
-router.delete('/:postId/comment/:commentId', authenticateAccessToken, postController.deleteCrewPostComment); //인증 필요
+router.post('/:postId/comment', authenticateAccessToken, verifyUserIsActive, postController.createCrewPostComment); //인증 필요
+router.put('/:postId/comment/:commentId', authenticateAccessToken, verifyUserIsActive, postController.updateCrewPostComment); //인증 필요
+router.delete('/:postId/comment/:commentId', authenticateAccessToken, verifyUserIsActive, postController.deleteCrewPostComment); //인증 필요
 
 export default router;
